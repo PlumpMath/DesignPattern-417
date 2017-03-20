465d2d69df65541a613d740a66be00d1d6ae3d76
xedy.zhang <xedy_zhang@126.com>
2017-03-20 10:45:38 +0800

    定义操作符；定义标点符号；定义语句栈。

--- DesignPattern/src/com/xedy/behaviroal/interpreter/Context.java
+++ DesignPattern/src/com/xedy/behaviroal/interpreter/Context.java
@@ -3,17 +3,46 @@
  */
 package com.xedy.behaviroal.interpreter;
 
+import java.util.Stack;
+
 /**
  * @author xedy_zhang@126.com
  *
  */
 public class Context {
 	/**
-	 * 此预研操作符
-	 *
+	 * 此操作符
 	 */
 	public enum OPERATORS{
-		_PUT_,_RMV_,_CACHE_,_SAVE_
+		$PUT("$put"),
+		$RMV("$RMV"),
+		$CACHE("$CACHE"),
+		$SAVE("$SAVE");
+		
+		private String value;
+		
+		OPERATORS(String value){
+			this.value = value;
+		}
+		
+		public String toString(){
+			return this.value;
+		}
 	}
 	
+	/**
+	 * 断句的标点符号
+	 */
+	public enum PUNCTUATION{
+		_BLANK(" "),
+		_LINE("\r\n");
+		
+		private String symbol;
+		
+		PUNCTUATION(String symbol){
+			this.symbol = symbol;
+		}
+	}
+	
+	private Stack<Object> sentenceStack;
 }
